7a7a218718e918f8d232cb3085b413f776d0e976
dariathompson <daria.kambalina@gmail.com>
2020-03-04 18:16:37 +0000

    implement hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,14 @@
 feature 'attacking another' do
-  scenario 'can attack another player and get confirmation' do
+   scenario 'can attack another player and get confirmation' do
+     sign_in_and_play
+     click_link 'Attack!'
+   expect(page).to have_content 'Sophia attacked Daria'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_link 'Attack!'
-    expect(page).to have_content 'Sophia attacked Daria'
+    expect(page).not_to have_content 'Daria: 60HP'
+    expect(page).to have_content 'Daria: 50HP'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,31 @@
 require 'player'
 
 describe Player do
+    subject(:sophia) {Player.new('Sophia')}
     subject(:daria) {Player.new('Daria')}
 
     describe '#name' do
       it 'stores the name' do
         expect(daria.name).to eq 'Daria'
+      end
+    end
+
+    describe '#hit_points' do
+      it 'returns the hit points' do
+        expect(sophia.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+      end
+    end
+
+    describe '#attack' do
+      it 'damages the player' do
+        expect(daria).to receive(:receive_damage)
+        sophia.attack(daria)
+      end
+    end
+
+    describe '#receive_damage' do
+      it 'reduces the player hit points' do
+        expect {daria.receive_damage}.to change {daria.hit_points}.by(-10)
       end
     end
 end
